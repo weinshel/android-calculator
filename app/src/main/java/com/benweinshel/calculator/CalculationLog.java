bd0348f74d88ebd24f8136d31ccf77c88bb16020
Ben Weinshel <weinshel@users.noreply.github.com>
2015-05-27 21:48:35 -0500

    added a class that git didn't seem to like

---
+++ app/src/main/java/com/benweinshel/calculator/CalculationLog.java
@@ -0,0 +1,17 @@
+package com.benweinshel.calculator;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * Created by bmweinshel15 on 5/26/15.
+ */
+public class CalculationLog {
+    String input;
+    String result;
+
+    CalculationLog(String input, String result) {
+        this.input = input;
+        this.result = result;
+    }
+}
